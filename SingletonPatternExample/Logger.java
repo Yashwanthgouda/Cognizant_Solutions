355d7f7c6b895033e2401442997463b402198307
Yashwanthgouda <syashwanthgouda@gmail.com>
2025-06-15 11:52:59 +0530

    Add Singleton Pattern Example

---
+++ SingletonPatternExample/Logger.java
@@ -0,0 +1,17 @@
+package SingletonPatternExample;
+
+public class Logger {
+    private static final Logger instance = new Logger();
+
+    private Logger() {
+        System.out.println("Logger Initialized");
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("[LOG] " + message);
+    }
+}
